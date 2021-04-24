ace743d6f343c25ba451bd52177dfb4a023b6284
Corey Innis <corey@geometer.io>
2021-04-24 11:26:31 -0700

    Default target

--- src/spec/basic.spec.ts
+++ src/spec/basic.spec.ts
@@ -4,15 +4,14 @@ import * as playwright from "playwright";
 describe('something', () => {
   it('does something', async () => {
     try {
-      await axios.default.get('http://localhost:8081/api/connect')
+      await axios.default.get('http://localhost:8081/api/connect/chromium')
         .then(async (res) => {
           console.info(`connecting to ${res.data.endpoint}`);
 
           const endpoint = res.data.endpoint;
           const browser  = await playwright.chromium.connect({ wsEndpoint: endpoint });
           const page     = await browser.newPage();
-          // let domain     = "amazon.com";
-          const target   = process.env.TARGET;
+          const target   = process.env.TARGET || "google.com";
 
           await page.goto(`https://${target}`);
           await page.screenshot({
@@ -21,32 +20,9 @@ describe('something', () => {
 
           await browser.close();
       });
-    } catch (error) {
+    } catch (e) {
       console.error("FAIL!!!");
+      throw(e);
     }
   });
 });
-
-// Gotta do some cleanup next...
-//
-// ➜ lsof -i TCP | grep node
-// node        665 corey   25u  IPv4 0xa47564e46a45f9c1      0t0  TCP localhost:49322->localhost:57971 (ESTABLISHED)
-// node        665 corey   31u  IPv4 0xa47564e467e27499      0t0  TCP localhost:49322 (LISTEN)
-// node        665 corey   32u  IPv4 0xa47564e467e223d9      0t0  TCP localhost:49323 (LISTEN)
-// node        665 corey   34u  IPv4 0xa47564e468337651      0t0  TCP localhost:45623 (LISTEN)
-// node        665 corey   35u  IPv4 0xa47564e468336c39      0t0  TCP localhost:58962 (LISTEN)
-// node      89905 corey   23u  IPv6 0xa47564e464688609      0t0  TCP *:sunproxyadmin (LISTEN)
-// node      89905 corey   31u  IPv6 0xa47564e486d8d2c9      0t0  TCP *:61256 (LISTEN)
-// node      89905 corey   35u  IPv6 0xa47564e486d9a609      0t0  TCP *:61269 (LISTEN)
-// node      89905 corey   40u  IPv6 0xa47564e464364609      0t0  TCP *:61285 (LISTEN)
-// node      89905 corey   45u  IPv6 0xa47564e4672b0c69      0t0  TCP *:61385 (LISTEN)
-// node      89905 corey   50u  IPv6 0xa47564e486d8cc69      0t0  TCP *:61523 (LISTEN)
-//
-// # kill geo-playwright (API server), then...
-//
-// ➜ lsof -i TCP | grep node
-// node        665 corey   25u  IPv4 0xa47564e46a45f9c1      0t0  TCP localhost:49322->localhost:57971 (ESTABLISHED)
-// node        665 corey   31u  IPv4 0xa47564e467e27499      0t0  TCP localhost:49322 (LISTEN)
-// node        665 corey   32u  IPv4 0xa47564e467e223d9      0t0  TCP localhost:49323 (LISTEN)
-// node        665 corey   34u  IPv4 0xa47564e468337651      0t0  TCP localhost:45623 (LISTEN)
-// node        665 corey   35u  IPv4 0xa47564e468336c39      0t0  TCP localhost:58962 (LISTEN)
