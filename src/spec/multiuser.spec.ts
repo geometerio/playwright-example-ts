c9488a3faa2965f2dda4fc6ded974fcfc3825eab
Corey Innis <corey@geometer.io>
2021-04-30 13:07:18 -0700

    improved async and code structure

--- src/spec/multiuser.spec.ts
+++ src/spec/multiuser.spec.ts
@@ -1,35 +1,41 @@
 import * as playwright from "playwright";
 import "expect-playwright"
 
-describe('a multiuser video chat', () => {
+let browser: playwright.Browser;
+let users: Array<string>;
+
+beforeAll(async () => {
+  users = ["user-0", "user-1", "user-2", "user-3"];
+
+  const endpoint = "ws://localhost:3000/playwright";
+  const queryStr = ['--use-fake-device-for-media-stream', '--use-fake-ui-for-media-stream'].join("&");
+  browser = await playwright.chromium.connect({wsEndpoint: `${endpoint}?${queryStr}`});
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+describe('a multiuser video room', () => {
   it('succeeds', async () => {
-    const wsEndpoint = `ws://localhost:3000/playwright?${
-      ['--use-fake-device-for-media-stream', '--use-fake-ui-for-media-stream'].join('&')
-    }`;
-    const browser = await playwright.chromium.connect({wsEndpoint});
-    const room = "20210429-coreyti";
+    const date = getDate();
+    const slug = getSlug();
+    const room = `${date}-geo-${slug}`;
+    const steps = [joinRoom, quitRoom];
 
-    const step1 = async (page : playwright.Page, participant : string) => {
-      await joinRoom(page, room, participant)
-    };
-    const step2 = async (page : playwright.Page, participant : string) => {
-      await quitRoom(page, room, participant);
-    };
+    console.info(`room will be '${room}'`);
 
-    await Promise.all(["0", "1", "2", "3"].map(async (participant : string) => {
+    await Promise.all(users.map(async (user : string) => {
       const context = await browser.newContext();
       const page = await context.newPage();
 
-      await step1(page, participant);
-      await expect(page).toHaveSelectorCount('.remote-videos-container > span.videocontainer', 4);
-      await capture(page, `doc/results/${room}/participant-${participant}-step1.png`);
+      // for (var step of steps) {
+      // await step(page, room, user);
+      // }
 
-      await step2(page, participant);
-      await expect(page).toHaveSelectorCount('.remote-videos-container > span.videocontainer', 0);
-      await capture(page, `doc/results/${room}/participant-${participant}-step2.png`);
+      await steps[0](page, room, slug, user);
+      await steps[1](page, room, slug, user);
     }));
-
-    await browser.close();
   });
 });
 
@@ -37,28 +43,44 @@ async function capture(page: playwright.Page, path: string) {
   await page.screenshot({path: path});
 }
 
-async function joinRoom(page: playwright.Page, room: string, participant: string) {
-  return new Promise<void>(async (resolve) => {
-    await page.goto(`https://meet.jit.si/${room}`);
-    await page.fill("css=input.field", `participant-${participant}`);
-    await page.click("css=div[data-testid='prejoin.joinMeeting']");
+async function joinRoom(page: playwright.Page, room: string, slug: string, user: string) {
+  await page.goto(`https://meet.jit.si/${room}`);
+  await page.fill("css=input.field", `${user}`);
+  await page.click("css=div[data-testid='prejoin.joinMeeting']");
 
-    const title = await page.title();
-    expect(title).toMatch("20210429");
+  const title = await page.title();
+  expect(title).toMatch(slug);
 
-    resolve();
-  });
+  // TODO: Figure out how to wait for 4 active video streams. Take a look at
+  // BrowserContext.route, perhaps. Playwright's capabilities for interacting
+  // with WebSockets could also provide some clues. "Success" here is
+  // achieved when all of the screenshots show all of the participants as
+  // actively "streaming" (and there is no need for `sleep`).
+  await expect(page).toHaveSelectorCount('span.videocontainer', users.length);
+  await sleep(500);
+  await capture(page, `doc/results/${room}/${user}-join.png`);
+  await sleep(500);
 }
 
-async function quitRoom(page: playwright.Page, room: string, participant: string) {
-  return new Promise<void>(async (resolve) => {
-    await page.goto("https://meet.jit.si");
+async function quitRoom(page: playwright.Page, room: string, slug: string, user: string) {
+  await page.goto("https://meet.jit.si");
 
-    const title = await page.title();
-    expect(title).toBe("Jitsi Meet");
+  const title = await page.title();
+  expect(title).toBe("Jitsi Meet");
 
-    resolve();
-  });
+  await sleep(500);
+  await capture(page, `doc/results/${room}/${user}-quit.png`);
+  await sleep(500);
+}
+
+function getDate() {
+  const date = new Date();
+  return new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().split("T")[0].replace(/-/g, "");
+}
+
+function getSlug() {
+  const chars = Math.random().toString(36).substring(2, 6);
+  return(chars.charAt(0).toUpperCase() + chars.slice(1));
 }
 
 function sleep(ms: number) {
